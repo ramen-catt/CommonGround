164402e261f3c031ffa4d6800606becf3d1d0fec
Beeonardo <marive0309@gmail.com>
2026-04-16 17:15:48 -0500

    updated messaging functions

--- src/backend/Messagingservice_Store_notif/MessagingStorage.java
+++ src/backend/Messagingservice_Store_notif/MessagingStorage.java
@@ -1,30 +1,23 @@
-class MessagingService
-{
-    public void CreateMsgRec(int msgId, int senderId, int recieverId, String msg, Time date)
-    {
-        this.msgId = msgId;
-        this.senderId = senderId;
-        this.RecieverId = recieverId;
-        this.msgTxt = msgTxt;
-        this.date = date;//when message was sent
+package Messagingservice_Store_notif;
+
+class MessagingStorage {
+    public static void main(String args[]) {
+
     }
 
-    public String[] GetConversation(int senderId, int recieverId)//grabs all messages between senderId and recieverId and returns them in an array
-    {
-        
-    }
-
-    public String GetMessage(int senderId)//grabs message with senderId and returns it
+    public void GetConversation(int senderId, int recieverId)// grabs all messages between senderId and recieverId and
+                                                             // returns them in an array
     {
 
     }
 
-    public void DeleteMsgRec(int msgId)
+    public String GetMessage(int senderId)// grabs message with senderId and returns it
     {
 
     }
 
+    public void DeleteMsgRec(int msgId) {
 
-
+    }
 
 }
